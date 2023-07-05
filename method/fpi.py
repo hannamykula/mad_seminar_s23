725ed272310803bcb119e6d39b492bb2b9f228c3
Hanna Mykula <hanna.mykula@tum.de>
2023-07-05 23:03:25 +0200

    Cpu/Gpu tensors

--- method/fpi.py
+++ method/fpi.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 def create_mask_batch(batch, patch_center, patch_width, patch_interp):
     dims = batch.size()[2:4]
     mask_i = torch.zeros_like(batch)
@@ -34,16 +36,22 @@ def patch_ex_batch(batch1, batch2, brain_core=0.7):
     center_y = np.random.randint(offset, offset+core)
     patch_center = np.stack((center_x, center_y))
     patch_width = np.random.randint(min_width, max_width)
-
+    
     interpolation_factor = np.random.uniform(0.05, 0.95)
-  
+    # patch_center = np.stack((107, 67))
+    # patch_width = 15
+    # interpolation_factor = 0.09199909689225388
+    # print(f"Center {patch_center}, width {patch_width}, inter {interpolation_factor}")
     offset = 1E-5 # separate 0 pixels from background
     mask = create_mask_batch(batch1, patch_center, patch_width, interpolation_factor + offset)
-    patch_mask = np.clip(np.ceil(mask), 0, 1) # all pixels set to 1
+    # mask = mask.cpu()
+    patch_mask = np.clip(np.ceil(mask.cpu()), 0, 1) # all pixels set to 1
+    patch_mask = patch_mask.to(device)
     mask = mask - patch_mask * offset # get rid of offset
     mask_inv = patch_mask - mask
     zero_mask = 1 - patch_mask # zero in the region of the patch
-
+    # mask = mask.to("cuda")
+    # mask_inv = mask_inv.to("cuda")
     # Interpolate between patches
     patch_set1 = mask * batch1 + mask_inv * batch2
     patch_set2 = mask_inv * batch1 + mask * batch2
@@ -51,7 +59,15 @@ def patch_ex_batch(batch1, batch2, brain_core=0.7):
     patch_batch1 = batch1 * zero_mask + patch_set1
     patch_batch2 = batch2 * zero_mask + patch_set2
 
-    valid_label = np.any((patch_mask*batch1 != patch_mask*batch2).numpy())
-    label = valid_label[...,None] * mask_inv.numpy()
+    # valid_label = np.any((patch_mask*batch1 != patch_mask*batch2).numpy())
+    # label = valid_label[...,None] * mask_inv.numpy()
+    valid_label = patch_mask*batch1 != patch_mask*batch2
+    valid_label = valid_label.cpu()
+    # Create the label array with the same shape as the mask, initialized with zeros
+    label = np.zeros_like(mask.cpu())
 
-    return patch_batch1, patch_batch2, torch.Tensor(label)
+    # Assign the values from the mask where comparison_mask is True
+    mask = mask.cpu()
+    label[valid_label] = mask[valid_label]
+
+    return patch_batch1, patch_batch2, torch.Tensor(label).to(device)
